0db402ec30c453d84d8242067e9b07debc99bb5c
Taylor Williams <taylor@thewilliams.net>
2019-03-03 23:44:31 +0000

    Automatically backed up by Learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,5 +1,5 @@
 class FoursquareService
-  
+
   def authenticate!(client_id, client_secret, code)
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = client_id
@@ -11,7 +11,7 @@ class FoursquareService
     body = JSON.parse(resp.body)
     body["access_token"]
   end
-  
+
   def friends(token)
     resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
       req.params['oauth_token'] = token
